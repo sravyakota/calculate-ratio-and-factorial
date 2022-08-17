a1f5437cfb575b53575510f08947bfb7f5b4c028
sravyakota <crazysravs577@gmail.com>
2022-08-17 18:08:30 +0000

    caculate ratio and factorial

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -1,12 +1,10 @@
-const ratioOfTwoNumbers=require('../ratio/index');
-const factorialOfNumber=require('../factorial/index');
+const ratioOfTwoNumbers = require("../ratio/index");
+const factorialOfNumber = require("../factorial/index");
 
+function ratioAndFactorial(num1, num2, num3) {
+  const ratio = ratioOfTwoNumbers(num1, num2);
+  const factorial = factorialOfNumber(num3);
 
-
-const ratioAndFactorial(num1,num2,num3){
-    const ratio=ratioOfTwoNumbers(num1,num2);
-    const factorial=factorialOfNumber(num3);
-
-    return {ratio,factorial};
+  return { ratio, factorial };
 }
-module.exports=ratioAndFactorial;
+module.exports = ratioAndFactorial;
